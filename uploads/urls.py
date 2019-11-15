044a0e3e4e528a2180421b8014c47a2d17c38a21
ewelinaszw <50328377+ewelinaszw@users.noreply.github.com>
2019-11-15 16:11:35 +0100

    Niniejszym zatwierdzam wszystkie zmiany we wszystkich plikach

--- uploads/urls.py
+++ uploads/urls.py
@@ -5,8 +5,10 @@ from uploads.core import views
 
 
 urlpatterns = [
-    url('^$', views.home, name='home'),
-    url('data_analysis/', views.data_analysis, name='data_analysis'),
-    url('about/', views.about, name='about'),
-    url('contact/', views.contact, name='contact'),
+    url('^$', views.stronaglowna, name='stronaglowna'),
+    url('rozklad/', views.rozklad, name='rozklad'),
+    url('trawmaje/', views.tramwaje, name='tramwaje'),
+    url('jf/',views.jf,name="jf"),
+    url('fj/',views.fj,name="fj"),
+
 ]
